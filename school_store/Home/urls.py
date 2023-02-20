a5107d5a1e77f067e309bffcd971d97fb93978ab
JOshi kv <joshikv3705@gmail.com>
2023-02-20 12:17:54 +0530

    completed

--- school_store/Home/urls.py
+++ school_store/Home/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.home_page,name='home'),
+    path('contact/',views.contact,name='contact'),
+    path('enquiry/',views.enquiry,name='enquiry'),
+    path('get_courses/<int:dpt_id>',views.get_courses,name='get_courses'),
     path('signup/',views.signup,name='signup'),
     path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout')
 ]
--- school_store/Home/views.py
+++ school_store/Home/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from . models import Departments,Courses
 from django.contrib.auth.models import User
@@ -6,6 +7,27 @@ from django.contrib import messages,auth
 def home_page(request):
     departments=Departments.objects.all()
     return render(request,'index.html',{'departments':departments})
+
+def contact(request):
+    return render(request,'contact.html')
+
+def enquiry(request):
+    departments=Departments.objects.all()
+    context={
+        'departments':departments,
+    }
+    if request.method=='POST':
+        option=request.POST.get('purpose')
+        if option == 'order':
+            messages.info(request,'Order confirmed')
+        else:
+            messages.info(request,'We will contact you soon')
+    return render(request,'enquiry.html',context)
+
+def get_courses(request,dpt_id):
+    courses=Courses.objects.filter(department_id=dpt_id)
+    data = [{'id': c.id, 'name': c.name} for c in courses]
+    return JsonResponse(data,safe=False)
 
 #function for signup
 
@@ -34,10 +56,13 @@ def login(request):
         user=auth.authenticate(username=username,password=password)
         if user is not None:
             auth.login(request,user)
-            return redirect('home')
+            return redirect('contact')
         else :
             messages.error(request,'Invalid Username or Password')
             return redirect('login')
     return render(request,'login.html')
 
 # function to log out
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
